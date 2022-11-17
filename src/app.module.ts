a435b037c271fae3ff7f089480007a4265ec8345
Juan Chaparro <chap800769@siigo.com>
2022-11-17 13:51:55 -0500

    first approach architecture

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { ContextModule } from './context/context.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ContextModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/context/context.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ApiContextModule } from './api/api-context.module';
+import { ApplicationContextModule } from './application/application-context.module';
+import { DomainContextModule } from './domain/domain-context.module';
+import { InfrastructureContextModule } from './infrastructure/infrastructure-context.module';
+
+@Module({
+    imports: [
+        ApiContextModule,
+        ApplicationContextModule,
+        DomainContextModule,
+        InfrastructureContextModule,
+    ],
+    controllers: [],
+    providers: [],
+})
+export class ContextModule {}
